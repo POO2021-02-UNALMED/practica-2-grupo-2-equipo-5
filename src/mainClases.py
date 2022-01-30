6f9c52378a4dd92664200bdd5fc30ce643d0e6cd
Valentín Osorio <dosoriom@unal.edu.co>
2022-01-30 14:15:25 -0500

    creado casos de prueba

--- src/mainClases.py
+++ src/mainClases.py
@@ -16,12 +16,34 @@ from gestionAplicacion.servicios.tipo_servicio import TipoServicio
 
 if __name__ == "__main__":
 
-    #Creacion de instancias de prueba
+    ## CREACION OBJETOS DE PRUEBA
+
+    # Clientes
     cliente1 = Cliente(nombre="Valentin Osorio",fecha_nacimiento="13/04/2000", 
                        direccion="Calle 30 #35-42", telefono= 3137865160)
+    cliente2 = Cliente(nombre="Andres Monsalve", fecha_nacimiento="24/05/2000")
+    cliente3 = Cliente(nombre="Fredy Orozco", fecha_nacimiento="08/6/2000", telefono=3212103863)
+
+    # ver los clientes por consola
+    for cedula, cliente in Cliente.getClientes().items():
+        print(cedula)
+        print(cliente)
+
+    # Cajeros
     cajero1 = Cajero(nombre="Alberto Guzman", sueldo=2500, comision = 0.0, 
-                     correo="albergu@unal.edu.co", cantidadEnVentas=3000, 
+                     correo="albergu@pjtech.com", cantidadEnVentas=3000, 
                      numeroContacto=32121)
+    cajero2 = Cajero(nombre="Pedro Jaramillo", sueldo=1000.8, comision= 0.05, 
+                     correo="pjaramillo@pjtech.com", cantidadEnVentas=3000)
+    cajero3 = Cajero(nombre="Adrian Perez", sueldo=600, comision=0.02, 
+                     cantidadEnVentas=0, numeroContacto=3457654423)
+
+    # Tecnicos
+    tecnico1 = Tecnico(nombre="Juan Gomez", sueldo=1500, comision = 0.0, experiencia=4)
+    tecnico2 = Tecnico()
+
+
+    # ver los cajeros por consola
     compra1 = CompraProductos(descripcion="Hello", descuento=0.05, 
                               fecha_de_compra="30/01/2022", devolucion=False)
     
@@ -30,7 +52,3 @@ if __name__ == "__main__":
     compra1.setCliente(cliente1)
     cajero1.agregarCompraProductos(compra1)
     compra1.setCajero(cajero1)
-
-    print(compra1)
-    print(compra1.getCliente())
-    print(compra1.getCajero())
